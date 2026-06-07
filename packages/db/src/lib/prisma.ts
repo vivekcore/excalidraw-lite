8d184743faafbe0cbd3907884dce9bd771e98b74
vivekcore <vivek.inz8@gmail.com>
2026-06-07 07:49:08 +0000

    added pooling in dg uri

--- packages/db/src/lib/prisma.ts
+++ packages/db/src/lib/prisma.ts
@@ -1,7 +1,12 @@
 import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config"
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+  max: 5,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 10000,
+});
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
