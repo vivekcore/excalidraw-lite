c9868226e6d991a94ecda21a0c60e782a747935c
vivekcore <vivek.inz8@gmail.com>
2026-06-23 18:12:20 +0000

    basic functionaly done

--- packages/db/src/lib/prisma.ts
+++ packages/db/src/lib/prisma.ts
@@ -1,20 +1,54 @@
-import { PrismaClient } from "../generated/prisma/client.js";
-import { PrismaPg } from "@prisma/adapter-pg";
-import "dotenv/config"
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-  max: 5,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 10000,
-});
+
+import { neonConfig } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import ws from 'ws'
+import { PrismaClient } from '../generated/prisma/client.js'
+
+neonConfig.webSocketConstructor = ws
+
+const createPrismaClient = () => {
+  const adapter = new PrismaNeon({
+    connectionString: process.env.DATABASE_URL!,
+  })
+
+  return new PrismaClient({ adapter })
+}
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+  prisma: PrismaClient | undefined
+}
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ adapter });
+  globalForPrisma.prisma ?? createPrismaClient()
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
 }
+
+export * from '@prisma/client' 
+
+
+
+
+
+
+// import { PrismaClient } from "../generated/prisma/client.js";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import "dotenv/config"
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL!,
+//   max: 5,
+//   idleTimeoutMillis: 30000,
+//   connectionTimeoutMillis: 10000,
+// });
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// export const prisma =
+//   globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+// if (process.env.NODE_ENV !== "production") {
+//   globalForPrisma.prisma = prisma;
+// }
