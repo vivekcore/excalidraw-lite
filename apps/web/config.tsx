8417fd3852ea3e738d257a483c2702dcbfa4ab67
vivekcore <vivek.inz8@gmail.com>
2026-06-24 12:06:26 +0000

    cleanup

--- apps/web/config.tsx
+++
@@ -1,4 +0,0 @@
-export const DATABASE_URL = "http://localhost:8000/api/v1"
-
-export const WS_URL = "ws://localhost:8080"
-
--- apps/web/utils/token.tsx
+++
@@ -1,6 +0,0 @@
-"use client"
-
-export function getToken():string | null {
-    if(typeof window === "undefined") return null
-    return localStorage.getItem("token")
-}
