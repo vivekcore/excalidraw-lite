77e188ed2d7cb4b3bfcc719bf312b83f2e28c5be
vivekcore <vivek.inz8@gmail.com>
2026-06-25 07:46:28 +0000

    setup for migration

---
+++ apps/http-backend/src/services/room.services.tsx
@@ -0,0 +1,15 @@
+
+class RoomServices {
+
+   async createRoom(userId:string,name:string){
+
+   }
+   async updateRoom(userId:string,name:string){
+
+   }
+   async deleteRoom(userId:string){
+    
+   }
+}
+
+export const roomServices = new RoomServices()
