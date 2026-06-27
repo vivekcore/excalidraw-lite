f964171af0d6dd760ab7cc1e6e2b8e931659f0bc
vivekcore <vivek.inz8@gmail.com>
2026-06-27 13:01:27 +0000

    improving code base

--- apps/http-backend/src/services/user.service.tsx
+++ apps/http-backend/src/services/user.service.tsx
@@ -1,20 +1,112 @@
+import { userSignin, userSignup, userUpdata } from "@repo/common/types";
+import { prisma } from "@repo/db";
+import { Prisma } from "../../../../packages/db/dist/generated/prisma/client.js";
+import Jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { response } from "express";
 
+type createUser = Prisma.UserCreateInput;
 class UserServices {
-
-    async SignupUser(data:any){
-
+  async SignupUser(data: any) {
+    const parse = userSignup.safeParse(data);
+    if (!parse.success) {
+      return;
     }
-
-    async LoginUser(data:any) {
-
+    const { name, email, password } = parse.data;
+    const existingU = prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+    if (existingU !== null) {
+      return;
     }
-    async UpdateUser(data:any,userId:string){
+    const response = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+    const payload = { userId: response.id };
+    const token = Jwt.sign(payload, JWT_SECRET);
+    return {
+      user: {
+        id: response.id,
+        name: response.name,
+        email: response.email,
+        image: response.image,
+        createdAt: response.createdAt,
+      },
+      token,
+    };
+  }
 
+  async LoginUser(data: any) {
+    const parse = userSignin.safeParse(data);
+    if (!parse.success) {
+      return;
     }
-    async DeleteUser(userId:string){
-
+    const { email, password } = parse.data;
+    const response = await prisma.user.findFirst({
+      where: {
+        email,
+        password,
+      },
+    });
+    if (!response) {
+      return;
     }
+    const payload = { userId: response.id };
+    const token = Jwt.sign(payload, JWT_SECRET);
 
+    return {
+      user: {
+        id: response.id,
+        name: response.name,
+        email: response.email,
+        image: response.image,
+        createdAt: response.createdAt,
+        updatedAt: response.updatedAt,
+      },
+      token,
+    };
+  }
+  async UpdateUser(data: any, userId: string) {
+    const parse = userUpdata.safeParse(data);
+    if (!parse.success) {
+      return;
+    }
+    let userD;
+    if (parse.data.email || parse.data.password || parse.data.name) {
+      userD = parse.data;
+    }
+    if (!userD) {
+      return;
+    }
+    //need to fix
+    const resposne = await prisma.user.update({
+      where: { id: userId },
+      data: {
+        ...userD,
+      },
+    });
+  }
+
+  async DeleteUser(userId: string) {
+    const response = await prisma.user.delete({
+      where: {
+        id: userId,
+      },
+    });
+    return {
+      user: {
+        id: response.id,
+        name: response.name,
+        email: response.email,
+      },
+    };
+  }
 }
 
-export const userServices = new UserServices()
+export const userServices = new UserServices();
