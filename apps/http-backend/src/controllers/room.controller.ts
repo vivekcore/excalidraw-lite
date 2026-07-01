f2d9f503a73b8e90c46c54b1856ca187a1d8e3d2
vivekcore <vivek.inz8@gmail.com>
2026-07-01 16:18:36 +0000

    migration

--- apps/http-backend/src/controllers/room.controller.ts
+++ apps/http-backend/src/controllers/room.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Response, Request } from "express";
 import { catchAsync } from "../utils/AsyncApi.js";
 import { roomServices } from "../services/room.services.js";
+import { sl } from "zod/locales";
 
 export const roomController = {
   createRoom: catchAsync(
@@ -40,5 +41,36 @@ export const roomController = {
         message:"Room Deleted",
         data:response
     })
-  })
-};
+  }),
+  myRooms: catchAsync(async(req:Request,res:Response,next:NextFunction) => {
+    const userId = req.userId
+    const response = await roomServices.myRooms(userId)
+    res.status(200).json({
+        status:"success",
+        message:"My Rooms",
+        data:response
+    })
+  } ),
+  getChatByRoomId: catchAsync(async(req:Request,res:Response,next:NextFunction) => {
+    const roomId = Number(req.params.roomId)
+    const userId = req.userId
+    const response = await roomServices.getChatByRoomId(userId,roomId)
+  
+   res.status(200).json({
+        status:"success",
+        message:"Room Chats",
+        data:response
+    })
+}),
+  getRoombySlug: catchAsync(async(req:Request,res:Response,next:NextFunction) => {
+    const slug = req.params.slug as string
+    const userId = req.userId
+
+    const response = await roomServices.getRoomBySlug(userId,slug)
+     res.status(200).json({
+        status:"success",
+        message:"Room By Slug",
+        data:response
+    })
+  } ),
+}
--- apps/http-backend/src/routes/roomRoutes.ts
+++ apps/http-backend/src/routes/roomRoutes.ts
@@ -1,96 +1,12 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import { Middleware } from "../middleware/userrMiddleware.js";
-import { prisma } from "@repo/db";
-import { GenerateSlug } from "../utils/slug.js";
+import { roomController } from "../controllers/room.controller.js";
 const router: Router = Router();
 
-router.post("/create-room", Middleware, async (req: Request, res: Response) => {
-  try {
-    const userId = req.userId;
-    const name = req.body.name;
-    const slug = GenerateSlug();
-   const cSlug = (await slug).toString();
-    const room = await prisma.room.create({
-      data: {
-        slug: cSlug,
-        name: name || "Untitled",
-        adminId: userId,
-      },
-    });
-    res.status(200).json({
-      status: "success",
-      data: room,
-    });
-  } catch (error) {
-    res.status(400).json({
-      Error: error,
-    });
-  }
-});
-router.get("/chats/:roomId", async (req: Request, res: Response) => {
-  try {
-    const roomId = Number(req.params.roomId);
-    const message = await prisma.chat.findMany({
-      where: {
-        roomId,
-      },
-      include: {
-        user: {
-          select: {
-            name: true,
-          },
-        },
-      },
-      orderBy: {
-        id: "desc",
-      },
-      take: 50,
-    });
-    const messages = message.map((m) => {return m.message})
-    res.status(200).json({
-      status: "success",
-      data: messages
-    });
-  } catch (error) {
-    res.json({
-      error,
-    });
-  }
-});
-router.get("/slug/:slug", async (req: Request, res: Response) => {
-  try {
-    const slug = req.params.slug as string;
-    const room = await prisma.room.findFirst({
-      where: {
-        slug,
-      },
-    });
-    res.status(200).json({
-      status: "success",
-      data: room,
-    });
-  } catch (error) {
-    res.json({
-      error,
-    });
-  }
-});
-router.get("/my-rooms",async(req:Request,res:Response) => {
-  try {
-    const userId = req.userId;
-    const room = await prisma.room.findMany({
-      where:{
-        adminId:userId
-      }
-    })
-    res.status(200).json({
-      status:"success",
-      data:room
-    })
-  } catch (error) {
-    res.json({
-      error
-    })
-  }
-})
+router.post("/create-room", Middleware, roomController.createRoom);
+router.post("/delete",Middleware,roomController.deleteRoom)
+router.patch("/update",Middleware,roomController.updateRoom)
+router.get("/chats/:roomId",Middleware,roomController.getChatByRoomId);
+router.get("/my-rooms",Middleware,roomController.myRooms)
+router.get("/room/:slug",Middleware,roomController.getRoombySlug)
 export default router;
